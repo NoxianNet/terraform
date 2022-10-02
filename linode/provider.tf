99c7e71c315c6d1d5a8778d0812c6c9919444da6
Paul <paul@Jupiter.local>
2022-10-01 20:32:15 -0400

    Initial commit

---
+++ linode/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    linode = {
+      source = "linode/linode"
+      version = "1.29.2"
+    }
+  }
+}
+
+#provider "linode" {
+#  # Configuration options
+#}
+#
