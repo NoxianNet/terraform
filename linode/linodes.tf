8770bbdfda0877adee2eb6764215a2e65b982c4e
Paul <paul@noxian.net>
2022-10-04 01:16:23 -0400

    change linode type

--- linode/linodes.tf
+++ linode/linodes.tf
@@ -2,7 +2,7 @@ resource "linode_instance" "reverse_proxy_docker" {
   region          = var.default_region
   label           = "nginx-docker"
   tags            = [ "docker", "nginx", "reverse-proxy" ]
-  type            = "g6-standard-2"
+  type            = "g6-standard-1"
   authorized_keys = [ var.ssh_key ]
   image           = "linode/ubuntu22.04"
 }
