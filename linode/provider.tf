03ef6149af0e76fec11ff4e4f21caeb2a9791b93
Paul <paul@Jupiter.local>
2022-10-04 00:32:53 -0400

    linodes and provider update

--- linode/provider.tf
+++ linode/provider.tf
@@ -1,19 +1,15 @@
 terraform {
   required_providers {
-    linode = {
-      source  = "linode/linode"
-      version = "1.29.2"
-    }
+	linode = {
+	  source  = "linode/linode"
+	  version = "1.29.2"
+	}
   }
   cloud {
-    organization = "NoxianNet"
-    workspaces {
-      name = "terraform"
-    }
+	organization = "NoxianNet"
+	workspaces {
+	  name = "terraform"
+	}
   }
-}
-
-provider "linode" {
-  token = var.LINODE_TOKEN
 }
 
