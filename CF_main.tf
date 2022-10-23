438d2eb78683534eec7a297cba43e62168a76452
Paul <paul@Pauls-MBP.local.noxian.net>
2022-10-22 21:00:43 -0400

    added record for @ zone

--- CF_main.tf
+++ CF_main.tf
@@ -14,3 +14,11 @@ resource "cloudflare_record" "docker-instance-subdomains" {
   ttl      = 60
   value    = linode_instance.reverse_proxy_docker.ip_address
 }
+
+resource "cloudflare_record" "main_domain" {
+  name    = "signalproxy.net"
+  type    = "A"
+  zone_id = cloudflare_zone.signalproxy_net.id
+  value   = linode_instance.reverse_proxy_docker.ip_address
+  ttl     = 60
+}
